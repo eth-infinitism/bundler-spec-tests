aa7198e90d0235d6802a909b310409f10be4b705
shahafn <shahaflol@gmail.com>
2022-11-13 19:42:03 +0200

    fixing deps, readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# EIP4337 bundler compatibility tests.
+
+#### Prerequisites 
+
+Python version 3.8+
+PDM - python package and dependency manager version 2.2.1+
+
+#### Installation
+Run `pdm install`
+#### Running the tests
+
+While running your bundler, open another windown and run
+```sh
+.venv/bin/pytest --url <bundler_url> --chain-id <chain_id> --entry-point <enty_point>
+```
--- tests/test_rpc.py
+++ tests/test_rpc.py
@@ -5,7 +5,7 @@ See https://github.com/eth-infinitism/bundler
 
 import pytest
 import requests
-import jsons
+import json
 
 from .types import UserOperation
 
@@ -27,28 +27,28 @@ def test_eth_supportedEntryPoints(url, entry_point):
     assert response["id"] == 1234
 
 
-# @pytest.mark.skip
 def test_eth_sendUserOperation(url, entry_point):
-    print(url)
     userOp = UserOperation("0x0000000000000000000000000000000000000000",
                            0,
                            '',
                            '',
-                           1e5,
-                           1e5,
-                           1e5,
-                           1e5,
-                           1e5,
+                           100000,
+                           100000,
+                           100000,
+                           100000,
+                           100000,
                            '',
                            '')
     payload = {
         "method": "eth_sendUserOperation",
-        "params": [jsons.dump(userOp), entry_point],
+        "params": [json.dumps(userOp, default=vars), entry_point],
         "jsonrpc": "2.0",
         "id": 1234,
     }
+    print('userOp is', json.dumps(userOp, default=vars))
     response = requests.post(url, json=payload).json()
 
+    print("response is", response)
     assert response["result"] == ""
     assert response["jsonrpc"] == "2.0"
     assert response["id"] == 1234
