64ecdf611460fb83b39389205f853dc5a748b48a
shahafn <shahaflol@gmail.com>
2023-01-04 21:43:09 +0200

    getUserOperationByHash tests wip

--- tests/rpc/test_eth_getUserOperationByHash.py
+++ tests/rpc/test_eth_getUserOperationByHash.py
@@ -4,22 +4,19 @@ from tests.types import RPCRequest
 from tests.utils import userOpHash, assertRpcError
 
 
-@pytest.mark.skip
 @pytest.mark.usefixtures("sendUserOperation")
-@pytest.mark.parametrize("method", ["eth_getUserOperationTransactionByHash"], ids=[""])
-def test_eth_getUserOperationTransactionByHash(wallet_contract, userOp, schema):
+@pytest.mark.parametrize("method", ["eth_getUserOperationByHash"], ids=[""])
+def test_eth_getUserOperationByHash(wallet_contract, userOp, schema):
     response = RPCRequest(
-        method="eth_getUserOperationTransactionByHash",
+        method="eth_getUserOperationByHash",
         params=[userOpHash(wallet_contract, userOp)],
     ).send()
+    print("response is", response)
     assert response.result["userOpHash"] == userOpHash(wallet_contract, userOp)
     Validator.check_schema(schema)
     validate(instance=response.result, schema=schema)
 
 
-@pytest.mark.skip
-def test_eth_getUserOperationTransactionByHash_error():
-    response = RPCRequest(
-        method="eth_getUserOperationTransactionByHash", params=[""]
-    ).send()
+def test_eth_getUserOperationByHash_error():
+    response = RPCRequest(method="eth_getUserOperationByHash", params=[""]).send()
     assertRpcError(response, "Missing/invalid userOpHash", -32601)
