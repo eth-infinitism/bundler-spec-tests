8e074a05891e644b7891bd67e1c31773f4968dc8
Alex Forshtat <forshtat1@gmail.com>
2023-08-17 12:14:49 +0300

    Use assembly to execute `EXTCODECOPY` without `EXTCODESIZE` first

--- tests/contracts/TestRulesAccount.sol
+++ tests/contracts/TestRulesAccount.sol
@@ -110,8 +110,22 @@ contract TestRulesAccount is IAccount, IPaymaster, Stakable {
         else if (eq(rule, "EXTCODESIZE")) return address(100500).code.length;
         else if (eq(rule, "EXTCODEHASH")) return uint256(address(100600).codehash);
         else if (eq(rule, "EXTCODECOPY")) {
-            bytes memory code = address(100700).code;
-            return 0;
+            bytes memory o_code;
+            assembly {
+            // retrieve the size of the code, this needs assembly
+            // let size := extcodesize(_addr)
+                let size := 0
+            // allocate output byte array - this could also be done without assembly
+            // by using o_code = new bytes(size)
+                o_code := mload(0x40)
+            // new "memory end" including padding
+                mstore(0x40, add(o_code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+            // store length in memory
+                mstore(o_code, size)
+            // actually retrieve the code, this needs assembly
+                extcodecopy(create2address, add(o_code, 0x20), 0, size)
+            }
+            return o_code.length;
         }
         else if (eq(rule, "SELFDESTRUCT")) {
             coin.destruct();
--- tests/contracts/TestRulesFactory.sol
+++ tests/contracts/TestRulesFactory.sol
@@ -34,8 +34,22 @@ contract TestRulesFactory is Stakable {
             emit Uint(uint256(create2address.codehash));
         }
         else if (rule.eq("EXTCODECOPY_SENDER")) {
-            bytes memory code = create2address.code;
-            emit Uint(code.length);
+            bytes memory o_code;
+            assembly {
+                // retrieve the size of the code, this needs assembly
+                // let size := extcodesize(_addr)
+                let size := 0
+                // allocate output byte array - this could also be done without assembly
+                // by using o_code = new bytes(size)
+                o_code := mload(0x40)
+                // new "memory end" including padding
+                mstore(0x40, add(o_code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+                // store length in memory
+                mstore(o_code, size)
+                // actually retrieve the code, this needs assembly
+                extcodecopy(create2address, add(o_code, 0x20), 0, size)
+            }
+            emit Uint(o_code.length);
         }
 
         account = new SimpleWallet{salt : bytes32(nonce)}(_entryPoint);
