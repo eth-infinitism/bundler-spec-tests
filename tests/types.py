043cf575b9b50308d5c24aa509ae8f5fe4dc3d97
Dror Tirosh <dror.tirosh@gmail.com>
2023-10-09 14:13:44 +0300

    increase default maxFeePerGas
    this is only the default, for tests unrelated to gas checks.
    separately, should add tests that verify proper use of maxFeePerGas
    (currently, this affects voltaire bundler)

--- tests/types.py
+++ tests/types.py
@@ -39,7 +39,7 @@ class UserOperation:
     callGasLimit: HexStr = hex(3 * 10**5)
     verificationGasLimit: HexStr = hex(10**6)
     preVerificationGas: HexStr = hex(3 * 10**5)
-    maxFeePerGas: HexStr = hex(2 * 10**9)
+    maxFeePerGas: HexStr = hex(4 * 10**9)
     maxPriorityFeePerGas: HexStr = hex(1 * 10**9)
     paymasterAndData: HexStr = "0x"
     signature: HexStr = "0x"
