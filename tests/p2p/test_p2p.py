8d19cdb2de8dc114343784c6f5dc60a509cea4ee
Dror Tirosh <dror.tirosh@gmail.com>
2023-10-30 11:35:24 +0200

    split test folders - tests/one, tests/p2p

--- tests/p2p/test_p2p.py
+++ tests/p2p/test_p2p.py
@@ -11,20 +11,18 @@ from tests.utils import (
 )
 
 
-if os.environ.get("P2P") is None:
-    pytest.skip(allow_module_level=True)
-
-#todo: relies on 'ports: [ "3001:3000" ]' in peer bundler.
+#todo: relies on 'ports: [ "3001:3000" ]' for peer bundler.
 BUNDLER2 = "http://localhost:3001/rpc"
 
-#todo: this can only work if the script runs inside the docker-compose environment.
+#todo: this is the "real" bundler2 definition. 
+# However, it can only work if the script runs inside the docker-compose environment.
 # BUNDLER2 = "http://bundler2:3000/rpc"
 
 #read mempool from the given bundler, after mempool propagation.
 # refDump - a "dump_mempool" taken from that bundler before the tested operation.
 # wait for the mempool to change before returning anything.
 def p2p_mempool(refDump, url=None):
-    count=50
+    count=5
     print("ref=", refDump)
     while True:
         newDump = dump_mempool(url)
@@ -34,6 +32,8 @@ def p2p_mempool(refDump, url=None):
         if count<=0:
             raise  Exception("timed-out waiting mempool change propagate to {0}".format(url))
         time.sleep(1)
+
+
 
 def test_simple_p2p(w3, entrypoint_contract, manual_bundling_mode):
     wallet = deploy_and_deposit(w3, entrypoint_contract, "SimpleWallet", False)
