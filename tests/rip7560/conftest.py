6ee383578ddd9781bae16d88e508ebfa2491bde6
Alex Forshtat <forshtat1@gmail.com>
2024-05-27 20:48:56 +0200

    Fix test errors of insufficient gas and insufficient gas price

--- tests/rip7560/conftest.py
+++ tests/rip7560/conftest.py
@@ -1,4 +1,6 @@
+import time
 import pytest
+
 from tests.rip7560.types import TransactionRIP7560
 from tests.utils import (
     deploy_contract,
@@ -7,17 +9,24 @@ from tests.utils import (
 
 @pytest.fixture
 def wallet_contract(w3):
-    return deploy_contract(
+    contract = deploy_contract(
         w3,
         "rip7560/TestAccount",
         value=0 * 10**18,
     )
+    time.sleep(0.1)
+    w3.eth.send_transaction(
+        {"from": w3.eth.accounts[0], "to": contract.address, "value": 10**18}
+    )
+    return contract
 
 
 @pytest.fixture
 def tx_7560(wallet_contract):
     return TransactionRIP7560(
         sender=wallet_contract.address,
+        maxFeePerGas=hex(100000000000),
+        maxPriorityFeePerGas=hex(100000000000),
         callData=wallet_contract.encodeABI(fn_name="anyExecutionFunction"),
         signature="0xface",
     )
