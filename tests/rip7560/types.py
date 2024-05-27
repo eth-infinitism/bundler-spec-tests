f2e92b1e950a7bddd00e321427e0df221776b345
shahafn <shahaflol@gmail.com>
2024-05-27 16:52:47 +0300

    Adding missing types.py

---
+++ tests/rip7560/types.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass, asdict
+
+from eth_typing import HexStr
+from eth_utils import to_checksum_address
+from tests.types import RPCRequest
+
+
+@dataclass
+class TransactionRIP7560:
+    # pylint: disable=too-many-instance-attributes, invalid-name
+    sender: HexStr
+    nonce: HexStr = hex(0)
+    factory: HexStr = "0x"
+    factoryData: HexStr = "0x"
+    callData: HexStr = "0x"
+    callGasLimit: HexStr = hex(3 * 10**5)
+    verificationGasLimit: HexStr = hex(10**6)
+    maxFeePerGas: HexStr = hex(4 * 10**9)
+    maxPriorityFeePerGas: HexStr = hex(3 * 10**9)
+    signature: HexStr = "0x"
+    paymaster: HexStr = "0x"
+    paymasterData: HexStr = "0x"
+    paymasterVerificationGasLimit: HexStr = hex(0)
+    paymasterPostOpGasLimit: HexStr = hex(0)
+    chainId: HexStr = hex(1337)
+    value: HexStr = hex(0)
+    accessList = "0x"
+    builderFee: HexStr = hex(0)
+
+    def __post_init__(self):
+        # pylint: disable=duplicate-code
+        self.sender = to_checksum_address(self.sender)
+        if self.paymaster is not None:
+            if self.paymasterVerificationGasLimit is None:
+                self.paymasterVerificationGasLimit = hex(10**5)
+            if self.paymasterPostOpGasLimit is None:
+                self.paymasterPostOpGasLimit = hex(10**5)
+            if self.paymasterData is None:
+                self.paymasterData = "0x"
+
+    def send(self, url=None):
+        return RPCRequest(method="eth_sendTransaction", params=[asdict(self)]).send(url)
