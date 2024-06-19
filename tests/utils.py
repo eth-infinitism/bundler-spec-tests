36c0f75321cdd80c044c3252aac9ecb9e5f04528
dancoombs <dan.coombs@alchemy.com>
2024-06-19 08:03:26 -0500

    chore: fix formatting again

--- tests/utils.py
+++ tests/utils.py
@@ -275,7 +275,7 @@ def sum_hex(*args):
 
 def get_userop_max_cost(user_op):
     return sum_hex(user_op.preVerificationGas,
-                user_op.verificationGasLimit,
-                user_op.callGasLimit,
-                user_op.paymasterVerificationGasLimit,
-                user_op.paymasterPostOpGasLimit) * to_number(user_op.maxFeePerGas)
+                   user_op.verificationGasLimit,
+                   user_op.callGasLimit,
+                   user_op.paymasterVerificationGasLimit,
+                   user_op.paymasterPostOpGasLimit) * to_number(user_op.maxFeePerGas)
