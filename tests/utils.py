876a3b5ed4ed832a9d2fefdc15c0fcadb4a926da
Dan Coombs <dan.coombs@alchemy.com>
2024-06-20 06:19:50 -0500

    fix(paymaster): fix paymaster deposit test (#79)
    * fix(paymaster): add a to_hex around VGL in test
    
    * fix(paymaster): fix paymaster deposit test
    
    * chore: fix lint
    
    * chore: fix formatting again

--- tests/utils.py
+++ tests/utils.py
@@ -273,8 +273,9 @@ def to_number(num_or_hex):
 def sum_hex(*args):
     return sum(to_number(i) for i in args if i is not None)
 
-
-def get_userop_verification_max_cost(user_op):
+def get_userop_max_cost(user_op):
     return sum_hex(user_op.preVerificationGas,
                    user_op.verificationGasLimit,
-                   user_op.paymasterVerificationGasLimit) * to_number(user_op.maxFeePerGas)
+                   user_op.callGasLimit,
+                   user_op.paymasterVerificationGasLimit,
+                   user_op.paymasterPostOpGasLimit) * to_number(user_op.maxFeePerGas)
