103d51b3c8104ec080fa7ff4a02a1f485eb902d7
Alex Forshtat <forshtat1@gmail.com>
2024-06-30 16:34:29 +0200

    Fix banned opcode test for Factory frame

--- tests/contracts/rip7560/RIP7560Deployer.sol
+++ tests/contracts/rip7560/RIP7560Deployer.sol
@@ -1,23 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.12;
 
-import "@openzeppelin/contracts/utils/Create2.sol";
+import "../Create2.sol";
+import "../ValidationRules.sol";
 
+import "./RIP7560TransactionStruct.sol";
 import {TestAccount} from "./TestAccount.sol";
 
-contract RIP7560Deployer {
+contract RIP7560Deployer is ValidationRulesStorage  {
 
-    uint256 public deplCounter = 0;
+    TestCoin public coin;
+    // uint256 public deplCounter = 0;
 
     event DeployerEvent(string name, uint256 counter, address deployed);
 
-    function createAccount(address owner, uint256 salt) external returns (address ret) {
+    function createAccount(address owner, uint256 salt, string memory rule) external returns (address ret) {
         ret = address(new TestAccount{salt : bytes32(salt)}());
-        emit DeployerEvent("the-deployer", deplCounter, ret);
-        deplCounter++;
+
+        ValidationRules.runRule(rule, this, coin, this);
+
+        // emit DeployerEvent("the-deployer", deplCounter, ret);
+        // deplCounter++;
     }
 
-    function getCreate2Address(address owner, uint256 salt) public view returns (address) {
+    function getCreate2Address(address owner, uint256 salt, string memory rule) public view returns (address) {
         return Create2.computeAddress(bytes32(salt), keccak256(abi.encodePacked(
             type(TestAccount).creationCode
         )));
--- tests/rip7560/test_rip7560.py
+++ tests/rip7560/test_rip7560.py
@@ -1,7 +1,10 @@
 import pytest
+
 from tests.single.opbanning.test_op_banning import banned_opcodes
 from tests.types import RPCErrorCode
 from tests.utils import send_bundle_now, assert_rpc_error, to_prefixed_hex
+
+from web3.constants import ADDRESS_ZERO
 
 
 def test_eth_sendTransaction7560(wallet_contract, tx_7560):
@@ -15,7 +18,7 @@ def test_eth_sendTransaction7560(wallet_contract, tx_7560):
 
 @pytest.mark.parametrize("banned_op", banned_opcodes)
 def test_account_eth_sendTransaction7560_banned_opcode(
-    wallet_contract_rules, tx_7560, banned_op
+        wallet_contract_rules, tx_7560, banned_op
 ):
     state_before = wallet_contract_rules.functions.state().call()
     assert state_before == 0
@@ -43,21 +46,26 @@ def test_paymaster_eth_sendTransaction7560_banned_opcode(
         response,
         "paymaster uses banned opcode: " + banned_op,
         RPCErrorCode.BANNED_OPCODE,
-        )
+    )
     send_bundle_now()
     state_after = wallet_contract.functions.state().call()
     assert state_after == 0
 
 
-
 @pytest.mark.parametrize("banned_op", banned_opcodes)
 def test_factory_eth_sendTransaction7560_banned_opcode(
-        wallet_contract_rules, tx_7560, factory_contract_7560, banned_op
+        w3, tx_7560, factory_contract_7560, banned_op
 ):
-    state_before = wallet_contract_rules.functions.state().call()
-    assert state_before == 0
-    tx_7560.sender = wallet_contract_rules.address
+    new_sender_address = factory_contract_7560.functions.getCreate2Address(
+        ADDRESS_ZERO, 123, banned_op
+    ).call()
+    tx_7560.sender = new_sender_address
+    code = w3.eth.get_code(new_sender_address)
+    assert code.hex() == ""
     tx_7560.factory = factory_contract_7560.address
+    tx_7560.factoryData = factory_contract_7560.functions.createAccount(
+        ADDRESS_ZERO, 123, banned_op
+    ).build_transaction()["data"]
     tx_7560.signature = to_prefixed_hex(banned_op)
     response = tx_7560.send()
     assert_rpc_error(
@@ -71,8 +79,9 @@ def test_factory_eth_sendTransaction7560_banned_opcode(
         RPCErrorCode.BANNED_OPCODE,
     )
     send_bundle_now()
-    state_after = wallet_contract_rules.functions.state().call()
-    assert state_after == 0
+    # no code deployed is the only check I can come up with here
+    code = w3.eth.get_code(new_sender_address)
+    assert code.hex() == ""
 
 #
 # def idfunction(case):
