4f8736b239b5183178d9a2e7868f0a0536ad2298
Alex Forshtat <forshtat1@gmail.com>
2024-08-12 19:59:17 +0200

    Add definition for 'sigFailPaymaster' callback function

--- tests/contracts/rip7560/interface/IRip7560EntryPoint.sol
+++ tests/contracts/rip7560/interface/IRip7560EntryPoint.sol
@@ -5,4 +5,5 @@ interface IRip7560EntryPoint {
     function acceptAccount(uint256 validAfter, uint256 validUntil) external;
     function sigFailAccount(uint256 validAfter, uint256 validUntil) external;
     function acceptPaymaster(uint256 validAfter, uint256 validUntil, bytes calldata context) external;
+    function sigFailPaymaster(uint256 validAfter, uint256 validUntil, bytes calldata context) external;
 }
