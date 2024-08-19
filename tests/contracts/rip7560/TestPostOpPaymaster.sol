16f64ea983566e62491133dfeeeb6eabf3687709
shahafn <shahaflol@gmail.com>
2024-08-19 14:50:10 +0300

    AA-336: Postop (#105)
    * paymaster postop tests
    
    * format
    
    * Lint
    
    * Adding asserts for execution phase state changes
    
    * Adding no_postop test when context is empty
    
    * Fixed post merge
    
    ---------
    
    Co-authored-by: Alex Forshtat <forshtat1@gmail.com>

---
+++ tests/contracts/rip7560/TestPostOpPaymaster.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.25;
+
+import "../Stakable.sol";
+import "./TestAccount.sol";
+import {IRip7560Paymaster} from "./lib/contracts/interfaces/IRip7560Paymaster.sol";
+import "../ValidationRules.sol";
+
+contract TestPostOpPaymaster is IRip7560Paymaster {
+    using ValidationRules for string;
+    uint256 public counter;
+    constructor() payable {}
+
+    function validatePaymasterTransaction(
+        uint256 version,
+        bytes32 txHash,
+        bytes calldata transaction)
+    external {
+        RIP7560TransactionStruct memory txStruct = RIP7560Utils.decodeTransaction(version, transaction);
+        bytes memory context = txStruct.signature;
+        if (string(context).eq("no context")) {
+            context = "";
+        }
+        RIP7560Utils.paymasterAcceptTransaction(context, 1, type(uint48).max - 1);
+    }
+
+    function postPaymasterTransaction(
+        bool success,
+        uint256 actualGasCost,
+        bytes calldata context
+    ) external {
+        string memory rule = string(context);
+        if (rule.eq("revert")) {
+            revert();
+        }
+        counter++;
+    }
+
+}
