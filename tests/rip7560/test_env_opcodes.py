a76c1eea491322d8b30b16fdfcbad5a33fc12ead
Alex Forshtat <forshtat1@gmail.com>
2024-09-08 21:18:43 +0200

    AA-402: Test environment introspection opcodes in every frame

--- tests/rip7560/test_env_opcodes.py
+++ tests/rip7560/test_env_opcodes.py
@@ -1,36 +1,120 @@
 import pytest
+from eth_utils import to_checksum_address
+
 from tests.rip7560.types import TransactionRIP7560
-from tests.utils import assert_ok, deploy_contract, send_bundle_now
+from tests.utils import (
+    assert_ok,
+    deploy_contract,
+    send_bundle_now,
+    get_rip7560_tx_max_cost,
+)
 
 AA_ENTRYPOINT = "0x0000000000000000000000000000000000007560"
 AA_SENDER_CREATOR = "0x00000000000000000000000000000000ffff7560"
 
 
-def test_side_effects(w3):
-    sender = deploy_contract(w3, "rip7560/TestAccountEnvInfo", value=1 * 10**18)
-    paymaster = deploy_contract(w3, "rip7560/TestPaymaster", value=1 * 10**18)
+def test_environment_introspection_opcodes(w3):
+    account_abi = deploy_contract(w3, "rip7560/OpcodesTestAccount")
+    factory = deploy_contract(w3, "rip7560/OpcodesTestAccountFactory")
+    paymaster = deploy_contract(w3, "rip7560/OpcodesTestPaymaster", value=1 * 10**18)
+
+    create_account_func = factory.functions.createAccount(1)
+    sender = create_account_func.call()
+    sender_contract = w3.eth.contract(abi=account_abi.abi, address=sender)
+
     tx = TransactionRIP7560(
-        sender=sender.address,
+        sender=sender,
         nonceKey=hex(0),
-        nonce=hex(1),
+        nonce=hex(0),
+        factory=factory.address,
+        factoryData=create_account_func.build_transaction()["data"],
         paymaster=paymaster.address,
-        maxFeePerGas=hex(100000000000),
-        maxPriorityFeePerGas=hex(12345),
+        maxFeePerGas=hex(400000000),
+        maxPriorityFeePerGas=hex(400000000),
         authorizationData="0xface",
-        executionData=sender.encodeABI(fn_name="saveEventOpcodes"),
-        # nonce = "0x1234"
+        executionData=account_abi.encodeABI(fn_name="saveEventOpcodes"),
     )
 
-    # tx = tx_7560
-    # assert tx_7560 == tx
-    ret = tx.send()
+    paymaster_balance_before = w3.eth.get_balance(paymaster.address)
+
+    ret = tx.send_skip_validation()
     assert_ok(ret)
     send_bundle_now()
-    event_args = sender.events.OpcodesEvent().get_logs()[0].args
-    assert event_args.tag == "exec"
+
+    account_opcode_events = sender_contract.events.OpcodesEvent().get_logs()
+    paymaster_opcode_events = paymaster.events.OpcodesEvent().get_logs()
+    factory_opcode_events = factory.events.OpcodesEvent().get_logs()
+    assert len(account_opcode_events) == 2
+    assert len(paymaster_opcode_events) == 2
+    assert len(factory_opcode_events) == 2
+
+    tmp_deployment_gas_cost = (
+        540000  # we should expose these values in the receipt and use the real ones
+    )
+    pre_charge = get_rip7560_tx_max_cost(tx)
+    # paymaster balance should not change within the transaction runtime
+    expected_paymaster_balance = paymaster_balance_before - pre_charge
+    tmp_remaining_account_validation_gas = hex(
+        int(str(tx.verificationGasLimit), 0) - tmp_deployment_gas_cost
+    )
+    validate_event(
+        w3,
+        tx,
+        "factory-validation",
+        factory.address,
+        AA_SENDER_CREATOR,
+        tx.verificationGasLimit,
+        factory_opcode_events[0].args,
+    )
+    validate_event(
+        w3,
+        tx,
+        "account-validation",
+        sender,
+        AA_ENTRYPOINT,
+        tmp_remaining_account_validation_gas,
+        account_opcode_events[0].args,
+    )
+    validate_event(
+        w3,
+        tx,
+        "paymaster-validation",
+        paymaster.address,
+        AA_ENTRYPOINT,
+        tx.paymasterVerificationGasLimit,
+        paymaster_opcode_events[0].args,
+        expected_paymaster_balance,
+    )
+    validate_event(
+        w3,
+        tx,
+        "account-execution",
+        sender,
+        AA_ENTRYPOINT,
+        tx.callGasLimit,
+        account_opcode_events[1].args,
+    )
+    validate_event(
+        w3,
+        tx,
+        "paymaster-postop",
+        paymaster.address,
+        AA_ENTRYPOINT,
+        tx.paymasterPostOpGasLimit,
+        paymaster_opcode_events[1].args,
+        expected_paymaster_balance,
+    )
+
+
+# pylint: disable=too-many-arguments
+def validate_event(
+    w3, tx, tag, entity_address, caller, gas, event_args, expected_balance=None
+):
+    assert event_args.tag == tag
     struct = dict(event_args.opcodes)
 
-    bal = w3.eth.get_balance(sender.address)
+    if expected_balance is None:
+        expected_balance = w3.eth.get_balance(entity_address)
     block = w3.eth.get_block("latest", True)
 
     tx_prio = int(tx.maxPriorityFeePerGas, 16)
@@ -41,14 +125,14 @@ def test_side_effects(w3):
         "TIMESTAMP": block.timestamp,
         "NUMBER": block.number,
         "CHAINID": 1337,
-        "GAS": pytest.approx(int(tx.callGasLimit, 16), rel=0.01),
+        "GAS": pytest.approx(int(gas, 16), rel=0.05),
         "GASLIMIT": block.gasLimit,
         "GASPRICE": min(tx_maxfee, tx_prio + block_basefee),
-        "BALANCE": bal,
-        "SELFBALANCE": bal,
-        "ORIGIN": sender.address,
-        "CALLER": AA_ENTRYPOINT,
-        "ADDRESS": sender.address,
+        "BALANCE": expected_balance,
+        "SELFBALANCE": expected_balance,
+        "ORIGIN": tx.sender,
+        "CALLER": to_checksum_address(caller),
+        "ADDRESS": entity_address,
         "COINBASE": block.miner,
         "BASEFEE": block.baseFeePerGas,
         "CALLVALUE": 0,
