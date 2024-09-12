edd7b8e32a335c1642518271e13e3a82444e8d00
Dror Tirosh <dror.tirosh@gmail.com>
2024-09-12 19:24:16 +0300

    test_gas_usage

--- tests/rip7560/test_gas_usage.py
+++ tests/rip7560/test_gas_usage.py
@@ -1,4 +1,3 @@
-import pytest
 from tests.rip7560.types import TransactionRIP7560
 from tests.utils import assert_ok, deploy_contract, send_bundle_now
 
@@ -6,7 +5,11 @@ AA_ENTRYPOINT = "0x0000000000000000000000000000000000007560"
 AA_SENDER_CREATOR = "0x00000000000000000000000000000000ffff7560"
 
 
-def test_gas_usage(w3, wallet_contract):
+def test_gas_usage_no_paymaster(w3, tx_7560):
+    send_and_check_payment(w3, tx_7560)
+
+
+def test_gas_usage_with_paymaster(w3, wallet_contract):
     paymaster = deploy_contract(w3, "rip7560/TestPaymaster", value=1 * 10**18)
     tx = TransactionRIP7560(
         sender=wallet_contract.address,
@@ -19,49 +22,54 @@ def test_gas_usage(w3, wallet_contract):
         executionData=wallet_contract.encodeABI(fn_name="anyExecutionFunction"),
         # nonce = "0x1234"
     )
+    send_and_check_payment(w3, tx)
 
+
+# check transaction payments:
+# - paymaster (or account) pays the gas
+# - coinbase gets the tip
+def send_and_check_payment(w3, tx: TransactionRIP7560):
+
+    if tx.paymaster is not None:
+        paymaster_pre_balance = w3.eth.get_balance(tx.paymaster)
+
+    wallet_pre_balance = w3.eth.get_balance(tx.sender)
     coinbase_pre_balance = w3.eth.get_balance(w3.eth.get_block("latest").miner)
-    # tx = tx_7560
-    # assert tx_7560 == tx
     ret = tx.send()
     assert_ok(ret)
     send_bundle_now()
 
-    bal = w3.eth.get_balance(wallet_contract.address)
+    if tx.paymaster is not None:
+        assert (
+            w3.eth.get_balance(tx.sender) == wallet_pre_balance
+        ), "wallet balance should not change"
     block = w3.eth.get_block("latest")
-
-    tx_prio = int(tx.maxPriorityFeePerGas, 16)
-    tx_max_fee = int(tx.maxFeePerGas, 16)
-    block_base_fee = block.baseFeePerGas
-
-    tx_gas_price = min(tx_max_fee, tx_prio + block_base_fee)
 
     coinbase_post_balance = w3.eth.get_balance(block.miner)
     coinbase_diff = coinbase_post_balance - coinbase_pre_balance
     # coinbase is paid for every tx in the block, and for each just the tip, not the full gas
     total_tx_tips = 0
+    total_tx_cost = 0
     for txhash in block.transactions:
         rcpt = w3.eth.get_transaction_receipt(txhash)
         actual_priority = rcpt.effectiveGasPrice - block.baseFeePerGas
         used = rcpt.gasUsed
-        cum = rcpt.cumulativeGasUsed
-        # TODO: gas_used field is broken for NORMAL tx (ok for type 4)
+        # todo: gas_used field is broken for NORMAL tx (ok for type 4)
         if used > rcpt.cumulativeGasUsed:
             used = 21000
+
+        if rcpt.type == 4:
+            tx_cost = used * rcpt.effectiveGasPrice
+            total_tx_cost = total_tx_cost + tx_cost
+
         tip = actual_priority * used
-        print(
-            "== tx gas",
-            used,
-            cum,
-            "tip",
-            tip,
-            "actual_priority",
-            actual_priority,
-            "basefee",
-            block.baseFeePerGas,
-        )
         total_tx_tips += tip
 
-    # total_tx_tips = sum([tx.maxPriorityFeePerGas for tx in block.transactions])
-
     assert coinbase_diff == total_tx_tips
+
+    if tx.paymaster is not None:
+        paymaster_paid = paymaster_pre_balance - w3.eth.get_balance(tx.paymaster)
+        assert paymaster_paid == total_tx_cost
+    else:
+        wallet_paid = wallet_pre_balance - w3.eth.get_balance(tx.sender)
+        assert wallet_paid == total_tx_cost
