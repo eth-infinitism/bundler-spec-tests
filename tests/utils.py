61a36b3919464afe3a39a8ccccde6eec7ec2cebf
Dror Tirosh <dror.tirosh@gmail.com>
2024-09-15 19:48:27 +0300

    dump_mempool lsupport both rip7560 and erc4337

--- tests/utils.py
+++ tests/utils.py
@@ -6,7 +6,9 @@ from eth_utils import to_checksum_address
 from eth_abi import decode
 from eth_abi.packed import encode_packed
 from solcx import compile_source
-from .types import RPCRequest, CommandLineArgs
+
+from .rip7560.types import TransactionRIP7560
+from .types import RPCRequest, UserOperation, CommandLineArgs
 
 
 @cache
@@ -226,7 +228,10 @@ def dump_mempool(url=None):
         .result
     )
     for i, entry in enumerate(mempool):
-        mempool[i] = dict(**entry)
+        if "executionData" in entry:
+            mempool[i] = TransactionRIP7560(**entry)
+        else:
+            mempool[i] = UserOperation(**entry)
     return mempool
 
 
