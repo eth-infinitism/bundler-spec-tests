1391f68a398de05d4fab53bb51a94e681e89abcb
Dror Tirosh <dror.tirosh@gmail.com>
2025-01-01 18:16:48 +0200

    AA-502: add EREP-030 (#133)
    * AA-502 add EREP-030

--- tests/single/reputation/test_erep.py
+++ tests/single/reputation/test_erep.py
@@ -1,6 +1,8 @@
 # extended reputation rules
-
 from dataclasses import dataclass
+
+import pytest
+from eth_utils import to_hex
 
 from tests.user_operation_erc4337 import UserOperation
 from tests.utils import (
@@ -10,30 +12,32 @@ from tests.utils import (
     dump_reputation,
     clear_reputation,
     set_reputation,
+    staked_contract,
     to_number,
     send_bundle_now,
-    dump_mempool,
     to_prefixed_hex,
 )
-from eth_utils import to_hex
 
 
 @dataclass
 class Reputation:
     address: str
-    opsSeen: int
-    opsIncluded: int
-    status: str
+    opsSeen: int = 0
+    opsIncluded: int = 0
+    status: int = 0
 
     def __post_init__(self):
         self.address = self.address.lower()
         self.opsSeen = to_number(self.opsSeen)
         self.opsIncluded = to_number(self.opsIncluded)
+        self.status = to_number(self.status)
 
 
-def get_reputation(addr):
+def get_reputation(addr, reputations=None):
+    if reputations is None:
+        reputations = dump_reputation()
     addr = addr.lower()
-    reps = [rep for rep in dump_reputation() if rep["address"].lower() == addr]
+    reps = [rep for rep in reputations if rep["address"].lower() == addr]
 
     if len(reps) == 0:
         rep = Reputation(address=addr, opsSeen=0, opsIncluded=0, status=0)
@@ -41,6 +45,11 @@ def get_reputation(addr):
         rep = Reputation(**reps[0])
 
     return rep
+
+
+def get_reputations(addrs):
+    reputations = dump_reputation()
+    return [get_reputation(addr, reputations) for addr in addrs]
 
 
 # EREP-015 A `paymaster` should not have its opsSeen incremented on failure of factory or account
@@ -109,3 +118,65 @@ def test_staked_factory_on_account_failure(
     factory.functions.setAccountState(0xDEAD).transact({"from": w3.eth.default_account})
     send_bundle_now()
     assert get_reputation(factory.address).opsSeen >= 10000
+
+
+# EREP-030 A Staked Account is accountable for failures in other entities (`paymaster`, `aggregator`) even if they are staked.
+@pytest.mark.parametrize("staked_acct", ["staked", "unstaked"])
+def test_account_on_entity_failure(
+    w3, entrypoint_contract, manual_bundling_mode, staked_acct, rules_account_contract
+):
+    clear_reputation()
+    # userop with staked account, and a paymaster.
+    # after submission to mempool, we will make paymaster fail 2nd validation
+    # (the simplest way to make the paymaster fail is withdraw its deposit)
+    sender = rules_account_contract
+    if staked_acct == "staked":
+        print("staking account", sender)
+        staked_contract(w3, entrypoint_contract, sender)
+    else:
+        print("unstaked account", sender)
+
+    paymaster = deploy_and_deposit(
+        w3, entrypoint_contract, "TestSimplePaymaster", False
+    )
+
+    assert get_reputations([sender.address, paymaster.address]) == [
+        Reputation(address=sender.address, opsSeen=0),
+        Reputation(address=paymaster.address, opsSeen=0),
+    ], "pre: no reputation"
+
+    assert_ok(UserOperation(sender=sender.address, paymaster=paymaster.address).send())
+
+    if staked_acct == "staked":
+        assert get_reputations([sender.address, paymaster.address]) == [
+            Reputation(address=sender.address, opsSeen=1),
+            Reputation(address=paymaster.address, opsSeen=1),
+        ], "valid userop. both staked account and paymaster have opsSeen increment"
+    else:
+        assert get_reputations([sender.address, paymaster.address]) == [
+            Reputation(address=sender.address, opsSeen=0),
+            Reputation(address=paymaster.address, opsSeen=1),
+        ], "valid userop. staked paymaster should have opsSeen increment"
+
+    # drain paymaster, so it would revert
+    pm_balance = entrypoint_contract.functions.balanceOf(paymaster.address).call()
+
+    tx_hash = paymaster.functions.withdrawTo(sender.address, pm_balance).transact(
+        {"from": w3.eth.default_account}
+    )
+    w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    bn = w3.eth.block_number
+    send_bundle_now()
+    assert w3.eth.block_number == bn, "bundle should revert, and not submitted"
+
+    if staked_acct == "staked":
+        assert get_reputations([sender.address, paymaster.address]) == [
+            Reputation(address=sender.address, opsSeen=1),
+            Reputation(address=paymaster.address, opsSeen=0),
+        ], "staked account should be blamed instead of paymaster"
+    else:
+        assert get_reputations([sender.address, paymaster.address]) == [
+            Reputation(address=sender.address, opsSeen=0),
+            Reputation(address=paymaster.address, opsSeen=1),
+        ]
