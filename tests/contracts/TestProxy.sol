475c3c72a07fb550c05445267063c58b987b3b5d
Dror Tirosh <dror.tirosh@gmail.com>
2025-02-04 12:05:41 +0200

    removed dependency on openzeppelin
    (inaccessible in our tests)

--- tests/contracts/TestProxy.sol
+++ tests/contracts/TestProxy.sol
@@ -1,16 +1,40 @@
 pragma solidity ^0.8;
 // SPDX-License-Identifier: GPL-3.0
 
-import "@account-abstraction/node_modules/@openzeppelin/contracts/proxy/Proxy.sol";
-
-contract TestProxy is Proxy {
+// based on openzeppelin's Proxy.sol";
+contract TestProxy  {
     address immutable impl;
 
     constructor(address _impl) {
         impl = _impl;
     }
 
-    function _implementation() internal view override returns (address) {
-        return impl;
+    function _delegate(address implementation) internal virtual {
+        assembly {
+        // Copy msg.data. We take full control of memory in this inline assembly
+        // block because it will not return to Solidity code. We overwrite the
+        // Solidity scratch pad at memory position 0.
+            calldatacopy(0, 0, calldatasize())
+
+        // Call the implementation.
+        // out and outsize are 0 because we don't know the size yet.
+            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
+
+        // Copy the returned data.
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegatecall returns 0 on error.
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    fallback() external payable virtual {
+        _delegate(impl);
     }
 }
